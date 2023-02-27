3036641e810cbbaca4dce50ad59f583e4f73efb9
neeraj1984 <joshi.neeraj84@gmail.com>
2023-02-27 10:30:17 +0530

    Add files via upload

---
+++ src/user/Login.js
@@ -0,0 +1,175 @@
+import React, { useState, useContext} from "react";
+import { useNavigate } from "react-router-dom";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../css/login.css";
+import lotus from "../img/lotus.jpeg";
+import axios from "axios";
+import AuthContext from "../store/auth-context";
+
+
+function Login(){
+
+//class Login extends React.Component{
+
+const loginURL = "http://localhost:8080/api/auth/signin";  
+// Declare a new state variable, which we'll call "password"
+const [password, setPassword] = useState("");
+const [email, setEmail] = useState("");
+const [passwordError, setpasswordError] = useState("");
+const [emailError, setemailError] = useState("");
+const [loginServiceError, setloginServiceError] = useState("");
+const navigate = useNavigate();
+const authCxt = useContext(AuthContext);
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': true,
+}
+
+const data = {
+  userName: email,
+  password: password,
+
+}
+
+function invokeLoginService(){
+
+  axios.post(
+          loginURL, data,
+          {headers:headers},
+        ).then((response) => {
+            if(response.data.accessToken !== ""){
+              //authCxt.login(response.data.token);
+              localStorage.setItem("user", JSON.stringify(response.data));
+              navigate("/home", {replace:true});
+            }else{
+              alert("some error occurred");
+            }
+    }).catch(error => {
+      if(error.response.status == 401){
+        setloginServiceError("Bad credentials.");
+      }else{
+        setloginServiceError("Some error has occurred.");
+      }     
+    });
+      
+}
+
+const loginSubmit = (e) => {
+    e.preventDefault();
+    var valid = handleValidation();
+    if(valid){
+      invokeLoginService();
+    }    
+  };
+
+  const handleValidation = (event) => {
+    let formIsValid = true;
+    if (!email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)) {
+      formIsValid = false;
+      setemailError("Email Not Valid");
+      return false;
+    } else {
+      setemailError("");
+      formIsValid = true;
+    }
+    // Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character
+    if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/)) {
+      
+      formIsValid = false;
+      setpasswordError(
+        "Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters."
+      );
+      return false;
+    } else {
+      setpasswordError("");
+      formIsValid = true;
+    }
+
+    return formIsValid;
+  };
+
+
+
+    return(           
+
+        <div className="h-100" style={{backgroundColor: "#eee"}}>
+            <div className="container h-100">
+              <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="col-xl-10">
+                  <div className="card rounded-3 text-black">
+                    <div className="row g-0">
+                      <div className="col-lg-6">
+                        <div className="card-body p-md-5 mx-md-4">
+
+                          <div className="text-center">
+                            <img src={lotus}
+                              style={{width: "185px"}} alt="logo" />
+                            <h4 className="mt-1 mb-5 pb-1">Voters Information System</h4>
+                          </div>
+
+                          <form id="loginform" onSubmit={loginSubmit}> 
+                            <p>Please login to your account</p>
+
+                            <div className="form-outline mb-4">
+                              <input type="email" id="emailId" className="form-control"
+                                placeholder="User name or email address" 
+                                onChange={(event) => setEmail(event.target.value)}/>
+                              <label className="form-label" htmlFor="formUserLabel">Username</label>
+                              <small id="emailHelp" className="text-danger form-text" style={{marginLeft:"10px"}}>
+                                  {emailError}
+                              </small>
+                            </div>
+
+                            <div className="form-outline mb-4">
+                              <input type="password" id="passwordId" className="form-control" 
+                              onChange={(event) => setPassword(event.target.value)}/>
+                              <label className="form-label" >Password</label>
+                                <small id="passwordErrorSection" className="text-danger form-text" style={{marginLeft:"10px"}}>
+                                  {passwordError}
+                                </small>
+                            </div>
+
+                            <small id="loginErrorSection" className="text-danger form-text" style={{marginLeft:"10px"}}>
+                                  {loginServiceError}
+                                </small>
+
+                            <div className="text-center pt-1 mb-5 pb-1">
+                              {/*
+                            <a className="text-muted" href="#!" style={{marginRight:"20px"}} onClick={() => navigate("/SignUp", {replace:true})}>Sign Up</a>
+                              */}
+                              <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" 
+                              type="submit">
+                                Log in</button>
+                              <a className="text-muted" href="#!" style={{marginLeft:"20px"}}>Forgot password?</a>
+                            </div>
+
+                            <div className="align-items-center justify-content-center pb-4">
+                              <p className="mb-0 me-2">Don't have an account?</p>
+                              <button type="button" className="btn gradient-custom-2" onClick={() => navigate("/SignUp", {replace:true})}>Create new</button>
+                            </div>
+
+                          </form>
+
+                        </div>
+                      </div>
+                      <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+                        <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                          <h4 className="mb-4">We are more than just a company</h4>
+                          <p className="small mb-0">VIS is one place hub for all the information related to voters.
+                          The systems contain: 1. Basic data, including data on electoral districts, municipalities, 
+                          voting districts and election authorities. 2. Data on polling</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    )
+}
+    
+
+
+export default Login;
---
+++ src/user/SignUp.js
@@ -0,0 +1,235 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "../css/signup.css";
+
+
+const signUpURL = "http://localhost:8080/api/auth/signup";  
+const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': true,
+  }
+
+
+
+class SignUp extends Component {
+
+
+
+    constructor(props) {
+        super(props);
+        this.handleRegister   = this.handleRegister.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.handleValidation = this.handleValidation.bind(this);
+        this.onChangeFirstName = this.onChangeFirstName.bind(this);
+        this.onChangeLastName = this.onChangeLastName.bind(this);
+        
+        this.state = {
+          username: "",
+          password: "",
+          firstName: "",
+          lastName: "",
+          successful: false,
+          usernameError: "",
+          passwordError: "",
+          firstNameError: "",
+          lastNameError: ""
+        };
+      }
+
+      onChangeUsername(e) {
+        this.setState({
+          username: e.target.value,
+        });
+      }
+    
+      onChangePassword(e) {
+        this.setState({
+          password: e.target.value,
+        });
+      }
+
+      onChangeFirstName(e) {
+        this.setState({
+          firstName: e.target.value,
+        });
+      }
+
+      onChangeLastName(e) {
+        this.setState({
+          lastName: e.target.value,
+        });
+      }
+
+    handleRegister(e) {
+
+        const data = {
+            userName: this.state.username,
+            password: this.state.password,
+            firstName:this.state.firstName,
+            lastName:this.state.lastName
+          }
+        //alert(this.state.username);
+        e.preventDefault();
+    
+        this.setState({
+          successful: false,
+        });
+        //in class component, we need to bind and use "this" to call method
+        var valid = this.handleValidation(); 
+        if(valid){
+
+            axios.post(
+                signUpURL, data,
+                {headers:headers},
+              ).then((response) => {
+                  alert(response.data.message);
+                  
+          }).catch(error => {
+            //alert(error);
+            if (!error.response) { //network error handling
+              alert(
+                "You can not sign up at this moment as service seems to be down."
+              );
+            }
+          });
+
+        }
+}
+
+handleValidation(e) {
+    let formIsValid = true;
+    if (!this.state.username.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)) {
+      formIsValid = false;
+      this.state.usernameError = "User name is not valid";
+      return false;
+    } else {
+      this.state.usernameError = "";
+      formIsValid = true;
+    }
+    // Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character
+    if (!this.state.password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/)) {
+      
+      formIsValid = false;
+      this.state.passwordError =
+        "Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters."
+      ;
+      return false;
+    } else {
+        this.state.passwordError = "";
+      formIsValid = true;
+    }
+
+    if(this.state.firstName == ""){
+      formIsValid = false;
+      this.state.firstNameError = "First Name is required";
+    }
+
+    if(this.state.lastName == ""){
+      formIsValid = false;
+      this.state.lastNameError = "Last Name is required";
+    }
+
+    return formIsValid;
+  }
+
+
+    render() {
+        const { message } = this.props;
+        return (
+
+            <div className="col-md-12">
+              <div className="card bg-light text-dark">
+      
+                <h1><center>User Registration </center></h1>
+      
+      
+                <form onSubmit={this.handleRegister}
+                  ref={(c) => {
+                    this.form = c;
+                  }}
+                >
+                  {!this.state.successful && (
+                    <div>
+                      <div className="form-group">
+                        <label htmlFor="username">Username/Email</label>
+                        <input type="text"
+                          className="form-control"
+                          name="username"
+                          value={this.state.username}
+                          style={{width: "25%"}}
+                          onChange={this.onChangeUsername}
+                        />
+                         <small id="emailHelp" className="text-danger form-text" style={{marginLeft:"10px"}}>
+                                  {this.state.usernameError}
+                              </small>
+                      </div>
+      
+                      <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          name="password"
+                          value={this.state.password}
+                          style={{width: "25%"}}
+                          onChange={this.onChangePassword}
+                        />
+
+                        <small id="passwordHelp" className="text-danger form-text" style={{marginLeft:"10px"}}>
+                                  {this.state.passwordError}
+                              </small>
+
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="firstName">First Name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          name="firstName"
+                          value={this.state.firstName}
+                          style={{width: "25%"}}
+                          onChange={this.onChangeFirstName}
+                        />
+
+                        <small id="firstNameHelp" className="text-danger form-text" style={{marginLeft:"10px"}}>
+                                  {this.state.firstNameError}
+                              </small>
+
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          name="lastName"
+                          value={this.state.lastName}
+                          style={{width: "25%"}}
+                          onChange={this.onChangeLastName}
+                        />
+
+                        <small id="lastNameHelp" className="text-danger form-text" style={{marginLeft:"10px"}}>
+                                  {this.state.lastNameError}
+                              </small>
+
+                      </div>
+      
+                      <div className="form-group">
+                        <button className="btn btn-dark btn-block">
+                                   Sign Up</button>
+                      </div>
+                    </div>
+                  )}
+
+                </form>
+              </div>
+            </div>
+          ); 
+    }
+
+}
+
+
+export default SignUp;
