b57753928fcd065425e8b9f33e698c27fc5671cc
neeraj1984 <joshi.neeraj84@gmail.com>
2023-02-27 10:27:53 +0530

    Create auth-context.js

---
+++ src/store/auth-context.js
@@ -0,0 +1,10 @@
+import React , {useState} from 'react';
+
+const AuthContext = React.createContext({
+    token: '',
+    isLoggedIn: false,
+    login: (token) => {},
+    logout: () => {}
+});
+
+export default AuthContext;
