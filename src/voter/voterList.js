7c0a7d05450dc8c8da8ef4c01410dc08d57e28e4
Neeraj Joshi <joshi.neeraj84@gmail.com>
2024-10-29 14:55:55 +0530

    added new css class

--- src/voter/voterList.js
+++ src/voter/voterList.js
@@ -1,12 +1,30 @@
-import React, { useState, useEffect} from "react";
-import { useNavigate } from "react-router-dom";
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import axios from 'axios';
+import { useTable,useFilters,usePagination} from 'react-table';
 import Header from "../user/header";
 
+{/* useMemo is a React Hook that lets you cache the result of a calculation between re-renders */}
 
-function VoterList(){
-    const navigate = useNavigate();
+// Define a default UI for filtering
+function DefaultColumnFilter({
+    column: { filterValue, preFilteredRows, setFilter },
+  }) {
+    const count = preFilteredRows.length;
+  
+    return (
+      <input
+        value={filterValue || ''}
+        onChange={e => {
+          setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+        }}
+        placeholder={`Search ${count} records...`}
+      />
+    );
+  }
+
+
+const VoterList = () => {
 
     const user = JSON.parse(localStorage.getItem("user"));
 
@@ -16,54 +34,160 @@ function VoterList(){
         'Access-Control-Allow-Origin': true,
         Authorization: "Bearer " + user.accessToken
       }
-    //this was causing issue to display the data, we have to store values as array[]
-    //const [voterResponse, getVoters] = useState(""); 
-    const [voterResponse, getVoters] = useState([]);
 
-      useEffect(() => {
-            getAllVoters();
-        }, []);
+    const [data, setData] = useState([]);
 
-    const  getAllVoters = () =>{
+    useEffect(() => {
         axios.get(listVoterURL,{headers:headers})
-            .then((response) => {
-                //alert("success:"+response.data[0].voterId); //correct way to debug response
-                const retval = response.data;
-                getVoters(retval);
+            .then(response => {
+                setData(response.data);
             })
-            .catch(error => (console.log("error")) )
-    }
+            .catch(error => {
+                console.error('Error fetching voter data:', error);
+            });
+    }, []);
 
-    return(
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Voter ID',
+                accessor: 'voterId', // accessor is the "key" in the data
+                Filter: DefaultColumnFilter,
+                
+            },
+            {
+                Header: 'Epic Number',
+                accessor: 'epicNumber',
+                Filter: DefaultColumnFilter,
+            },
+            {
+                Header: 'First Name',
+                accessor: 'firstName',
+                Filter: DefaultColumnFilter,
+            },
+            {
+                Header: 'Last Name',
+                accessor: 'lastName',
+                Filter: DefaultColumnFilter,
+            },
+        ],
+        []
+    );
+
+
+    // Use the useTable hook to create the table instance
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
+    } = useTable(
+        {
+            columns,
+            data,
+            initialState: { pageIndex: 0 }, // Set the initial page index
+        },
+        useFilters, // Use filters
+        usePagination // Use pagination
+    );
+
+    return (
+
         <div className="container">
         <Header></Header>
         <div className ="row mb-4"></div>
         <h2>Voter List</h2>
 
-        <div className="align-items-center justify-content-center pb-4">
-        {
-            <div className="row">
-                    <div className="pt-1 col-3">Voter ID </div>                   
-                    <div className="pt-1 col-3">Epic Number </div>
-                    <div className="pt-1 col-3">First Name </div>
-                    <div className="pt-1 col-3">Last Name</div>   
-            </div>
-        }
-        {
-            
-            voterResponse.map((item, i) => (
-                <div key={i} className="row">
-                    <div className="pt-1 col-3">{item.voterId} </div>                   
-                    <div className="pt-1 col-3">{item.epicNumber} </div>
-                    <div className="pt-1 col-3">{item.firstName} </div>
-                    <div className="pt-1 col-3">{item.lastName} </div>        
-                </div>
-            ))
-          }
-          
-         </div>
-        
+        <>
+        {/* ... is spread syntex - The Spread syntax is used to deconstruct an array or object into separate variables */}
+        <table {...getTableProps()} style={{ width: '90%', border: '1px solid black' }}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()} style={{ padding: '10px', border: '1px solid black' }}>
+                                    {column.render('Header')}
+                                    <div>
+                                        {column.canFilter ? column.render('Filter') : null}
+                                    </div>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map(row => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => (
+                                    <td {...cell.getCellProps()} style={{ padding: '10px', border: '1px solid black' }}>
+                                        {cell.render('Cell')}
+                                    </td>
+                                ))}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+
+        <div className="pagination" style={{ marginTop: '10px' }}>
+            <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                {'<<'}
+            </button>{' '}
+            <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                {'<'}
+            </button>{' '}
+            <button onClick={() => nextPage()} disabled={!canNextPage}>
+                {'>'}
+            </button>{' '}
+            <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                {'>>'}
+            </button>{' '}
+            <span>
+                Page{' '}
+                <strong>
+                    {pageIndex + 1} of {pageOptions.length}
+                </strong>{' '}
+            </span>
+            <span>
+                | Go to page:{' '}
+                <input
+                    type="number"
+                    defaultValue={pageIndex + 1}
+                    onChange={e => {
+                        const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                        gotoPage(page);
+                    }}
+                    style={{ width: '50px' }}
+                />
+            </span>{' '}
+            <select
+                value={pageSize}
+                onChange={e => setPageSize(Number(e.target.value))}
+            >
+                {[10, 20, 30, 40, 50].map(pageSize => (
+                    <option key={pageSize} value={pageSize}>
+                        Show {pageSize}
+                    </option>
+                ))}
+            </select>
         </div>
+    </>
+
+    </div>
+
     );
-}
+};
+
 export default VoterList;
